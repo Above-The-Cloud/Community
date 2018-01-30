ef988f9424fe6f8509f674337a6e10d57006da9b
Michael <yiwangchunyu@163.com>
2018-01-30 23:12:12 +0800

    init with no title(NoActionBar)

---
+++ README.txt
@@ -0,0 +1,5 @@
+IDE: AndroidStudio
+
+SDK Platform: Android 7.1.1     API level: 25
+
+Device Aquied: Android 4.0 
---
+++ app/src/main/java/wang/yiwangchunyu/community/MainActivity.java
@@ -0,0 +1,15 @@
+package wang.yiwangchunyu.community;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Window;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
