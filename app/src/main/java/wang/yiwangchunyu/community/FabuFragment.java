d0a9c0fbc3e32c3bae35f7110e70da8fb3c52aa8
xinyujiang <503273921@qq.com>
2018-03-15 01:28:23 +0800

    实现访问相册

--- app/src/main/java/wang/yiwangchunyu/community/FabuFragment.java
+++ app/src/main/java/wang/yiwangchunyu/community/FabuFragment.java
@@ -1,7 +1,11 @@
 package wang.yiwangchunyu.community;
 
+import android.annotation.TargetApi;
+import android.content.ContentUris;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.drawable.BitmapDrawable;
@@ -10,8 +14,12 @@ import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.provider.DocumentsContract;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.content.FileProvider;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -35,7 +43,6 @@ import java.io.IOException;
 
 import wang.yiwangchunyu.community.Task.Bimp;
 import wang.yiwangchunyu.community.Task.FileUtils;
-import wang.yiwangchunyu.community.Task.TestPicActivity;
 import wang.yiwangchunyu.community.dataStructures.TaskPublishingInfo;
 
 import static android.app.Activity.RESULT_OK;
@@ -157,9 +164,12 @@ public class FabuFragment extends Fragment{
             });
             bt2.setOnClickListener(new OnClickListener() {//相册
                 public void onClick(View v) {
-                    Intent intent = new Intent(getContext(),
-                            TestPicActivity.class);
-                    startActivity(intent);
+
+                    if (ContextCompat.checkSelfPermission(getActivity(), android.Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                        ActivityCompat.requestPermissions(getActivity(),new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE },1);
+                    }else {
+                        openAlbum();
+                    }
 
                     dismiss();
                 }
@@ -479,8 +489,72 @@ public class FabuFragment extends Fragment{
                     }
                 }
                 break;
+            case CHOOSE_PHOTO:
+                if (resultCode == RESULT_OK){
+                    //判断手机系统版本号
+                    if (Build.VERSION.SDK_INT >= 19){
+                        //4.4及以上系统使用这个方法处理图片
+                        handleImageOnKitKat(data);
+                    }else {
+                        //4.4以下系统使用这个方法处理图片
+                        handleImageBeforeKitKat(data);
+                    }
+                }
             default:
                 break;
+        }
+    }
+
+    @TargetApi(19)
+    private void handleImageOnKitKat(Intent data){
+        String imagePath = null;
+        Uri uri = data.getData();
+        if(DocumentsContract.isDocumentUri(getContext(),uri)){
+            //如果是document类型的Uri,则通过document id 处理
+            String docId = DocumentsContract.getDocumentId(uri);
+            if("com.android.providers.media.documents".equals(uri.getAuthority())){
+                String id = docId.split(":")[1];//解析出数字格式的id
+                String selection = MediaStore.Images.Media._ID + "=" + id;
+                imagePath = getImagePath(MediaStore.Images.Media.EXTERNAL_CONTENT_URI,selection);
+            }else if ("com.android.providers.downloads.documents".equals(uri.getAuthority())){
+                Uri contentUri = ContentUris.withAppendedId(Uri.parse("content://downloads/public_downloads"),Long.valueOf(docId));
+                imagePath = getImagePath(contentUri, null);
+            }
+        }else if ("content".equalsIgnoreCase(uri.getScheme())){
+            //如果是content类型的Uri,则使用普通方式处理
+            imagePath = getImagePath(uri, null);
+        } else if ("file".equalsIgnoreCase(uri.getScheme())){
+            //如果是file类型的Uri，直接获取图片路径即可
+            imagePath = uri.getPath();
+        }
+        displayImage(imagePath);//根据图片路径显示图片
+    }
+
+    private void handleImageBeforeKitKat(Intent data){
+        Uri uri = data.getData();
+        String imagePath = getImagePath(uri,null);
+        displayImage(imagePath);
+    }
+
+    private String getImagePath(Uri uri, String selection){
+        String path = null;
+        //通过Uri和selection来获取真实的图片路径
+        Cursor cursor = getActivity().getContentResolver().query(uri,null,selection,null,null);
+        if(cursor != null){
+            if (cursor.moveToFirst()){
+                path = cursor.getString(cursor.getColumnIndex(MediaStore.Images.Media.DATA));
+            }
+            cursor.close();
+        }
+        return path;
+    }
+
+    private void displayImage(String imagePath){
+        if (imagePath != null){
+            Bitmap bitmap = BitmapFactory.decodeFile(imagePath);
+            picture.setImageBitmap(bitmap);
+        }else {
+            Toast.makeText(getContext(),"failed to get image",Toast.LENGTH_SHORT).show();
         }
     }
 
@@ -536,30 +610,26 @@ public class FabuFragment extends Fragment{
         }*/
     }
 
-    /*protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        switch (requestCode) {
-            case TAKE_PICTURE:
-                if (Bimp.drr.size() < 9 && resultCode == -1) {
 
-                    // 拍照
-//				File f = new File(Environment.getExternalStorageDirectory() + "/" + localTempImgDir + "/" + localTempImgFileName);
-                    try {
-                        Uri u = Uri.parse(android.provider.MediaStore.Images.Media.insertImage(getContentResolver(), f.getAbsolutePath(), null, null));
-                        path = f.getPath();
-//					String URL = getRealFilePath(this, u);
-//					Intent intent = new Intent(MyHeadPicture.this, ClipImageActivity.class);
-//					intent.putExtra("bitmapURL", URL);
-//					startActivity(intent);
-                    } catch (FileNotFoundException e) {
-                        e.printStackTrace();
-                    }
+    private void openAlbum(){
+        Intent intent = new Intent("android.intent.action.GET_CONTENT");
+        intent.setType("image/*");
+        startActivityForResult(intent,CHOOSE_PHOTO);//打开相册
 
-                    Bimp.drr.add(path);
-                    adapter.notifyDataSetChanged();
-                    Toast.makeText(getContext(), Bimp.drr.size()+"", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode){
+            case 1:
+                if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    openAlbum();
+                } else {
+                    Toast.makeText(getActivity() , "You denied the permission",
+                            Toast.LENGTH_SHORT).show();
                 }
                 break;
+            default:
         }
-    }*/
-
+    }
 }
